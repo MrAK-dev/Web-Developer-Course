e33f391573b1a9f2643a8bd1fed460c23860d37b
Alex <alex.fiendish@gmail.com>
2023-08-14 20:37:20 +0300

    added html and css

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,12 @@
 import * as gulpFunctions from './gulpFunctions.js';
 gulpFunctions.isWebp();
+
+const body = document.querySelector('body');
+const burgerMenuIcon = document.querySelector('.icon-menu');
+const menuBody = document.querySelector('.menu__body');
+
+burgerMenuIcon.addEventListener('click', () => {
+  menuBody.classList.toggle('menu-open');
+  burgerMenuIcon.classList.toggle('menu-open');
+  body.classList.toggle('lock');
+});
