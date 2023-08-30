f9d648100df77f0705d1fca7ff476575d3e90ee2
Alex <alex.fiendish@gmail.com>
2023-08-30 19:17:22 +0300

    added full html, css

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,15 @@
 import * as gulpFunctions from './gulpFunctions.js';
 gulpFunctions.isWebp();
+
+const reviewsSwiper = document.querySelector('.swiper-reviews');
+
+if (reviewsSwiper) {
+  const swiper = new Swiper('.swiper-reviews', {
+    autoHeight: true,
+    loop: true,
+    pagination: {
+      el: '.swiper-reviews__pagination',
+      clickable: true,
+    },
+  });
+}
